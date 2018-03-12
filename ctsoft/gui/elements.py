c18694291ac963e9b45fc360adec6302d23abdb6
1stthomas <thomas.pluess@mesch.ch>
2018-03-12 20:49:54 +0100

    top level options implemented; inheritance expanded

--- ctsoft/gui/elements.py
+++ ctsoft/gui/elements.py
@@ -8,10 +8,22 @@ Created on Mon Mar  5 18:03:52 2018
 import tkinter as tk
 
 
-class TkGuiExtender(object):
-    def __init__(self, master, element):
-        super(TkGuiExtender, self).__init__()
+class TkBase(object):
+    def __init__(self):
+        super(TkBase, self).__init__()
         self.numerics = ["bd", "height", "ipadx", "ipady", "width"]
+
+    def setOptions(self, options):
+        for key in options:
+            if key in self.numerics:
+                self[key] = int(options[key])
+            else:
+                self[key] = options[key]
+
+
+class TkWidget(TkBase):
+    def __init__(self, master, element):
+        super(TkWidget, self).__init__()
         self.setParent(master)
         self.setOptions(element.attrib)
         self.doPack(element)
@@ -29,50 +41,80 @@ class TkGuiExtender(object):
     def getSelf(self):
         return self
 
-    def setOptions(self, options):
-        for key in options:
-            if key in self.numerics:
-                self[key] = int(options[key])
-            else:
-                self[key] = options[key]
-
     def setParent(self, parent):
         self.__parent = parent
 
 
-class TkButton(tk.Button, TkGuiExtender):
+class TkButton(tk.Button, TkWidget):
     def __init__(self, master, *args, **kw):
         tk.Button.__init__(self, master)
-        TkGuiExtender.__init__(self, master, *args, **kw)
+        TkWidget.__init__(self, master, *args, **kw)
 #        super(TkButton, self).__init__(self, master, *args, **kw)
 
 
-class TkEntry(tk.Entry, TkGuiExtender):
+class TkEntry(tk.Entry, TkWidget):
     def __init__(self, master, *args, **kw):
         tk.Entry.__init__(self, master)
-        TkGuiExtender.__init__(self, master, *args, **kw)
+        TkWidget.__init__(self, master, *args, **kw)
 #        super(TkEntry, self).__init__(self, master, *args, **kw)
 
 
-class TkFrame(tk.Frame, TkGuiExtender):
+class TkFrame(tk.Frame, TkWidget):
     def __init__(self, master, *args, **kw):
         tk.Frame.__init__(self, master)
-        TkGuiExtender.__init__(self, master, *args, **kw)
+        TkWidget.__init__(self, master, *args, **kw)
 #        super(TkFrame, self).__init__(master, *args, **kw)
 
 
-class TkLabel(tk.Label, TkGuiExtender):
+class TkLabel(tk.Label, TkWidget):
     def __init__(self, master, *args, **kw):
         tk.Label.__init__(self, master)
-        TkGuiExtender.__init__(self, master, *args, **kw)
+        TkWidget.__init__(self, master, *args, **kw)
 #        super(TkLabel, self).__init__(master, *args, **kw)
 
 
-class TkWindow(tk.Tk):
+class TkWindow(tk.Tk, TkBase):
     def __init__(self, element, *args, **kw):
         tk.Tk.__init__(self)
-        self.setOptions(element.attrib)
+        TkBase.__init__(self)
+        self.setOptions(dict(element.attrib))
 
     def setOptions(self, attributes):
+        if "maxsize-x" in attributes and "maxsize-y" in attributes:
+            self.maxsize(attributes["maxsize-x"], attributes["maxsize-y"])
+            del attributes["maxsize-x"]
+            del attributes["maxsize-y"]
+        elif "maxsize-x" in attributes:
+            self.maxsize(attributes["maxsize-x"], None)
+            del attributes["maxsize-x"]
+        elif "maxsize-y" in attributes:
+            self.maxsize(None, attributes["maxsize-y"])
+            del attributes["maxsize-y"]
+
+        if "minsize-x" in attributes and "minsize-y" in attributes:
+            self.minsize(attributes["minsize-x"], attributes["minsize-y"])
+            del attributes["minsize-x"]
+            del attributes["minsize-y"]
+        elif "minsize-x" in attributes:
+            self.minsize(attributes["minsize-x"], None)
+            del attributes["minsize-x"]
+        elif "minsize-y" in attributes:
+            self.minsize(None, attributes["minsize-y"])
+            del attributes["minsize-y"]
+
+        if "resizable-x" in attributes and "resizable-y" in attributes:
+            self.resizable(attributes["resizable-x"], attributes["resizable-y"])
+            del attributes["resizable-x"]
+            del attributes["resizable-y"]
+        elif "minsize-x" in attributes:
+            self.resizable(attributes["resizable-x"], None)
+            del attributes["resizable-x"]
+        elif "minsize-y" in attributes:
+            self.resizable(None, attributes["resizable-y"])
+            del attributes["resizable-y"]
+
         if "title" in attributes:
             self.title(attributes["title"])
+            del attributes["title"]
+
+        TkBase.setOptions(self, attributes)
